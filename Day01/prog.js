50c01bb8947f7176e5140f243fc437e84479aeee
TuhinCodeCraft <tuhinghosh0169@gmail.com>
2024-07-13 09:14:39 +0530

    Day 1 Challenge solved

---
+++ Day01/prog.js
@@ -0,0 +1,36 @@
+// Task 1:
+var num = 145
+console.log(num);
+
+// Task 2:
+let str = "Tuhin"
+console.log(str);
+
+// Task 3:
+const bool = true
+console.log(bool);
+
+// Task 4:
+const num2 = 12345
+const str2 = "Here is string"
+const isTrue = true
+const obj = {
+    1: "one",
+    2: "two"
+}
+const arr = [1,2,3,4,5]
+console.log(typeof num2);
+console.log(typeof str2);
+console.log(typeof isTrue);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Task 5:
+let ans = true
+console.log(`Initial Value: ${ans}`);
+ans = false
+console.log(`New Value: ${ans}`);
+
+// Task 6:
+const val = 250
+val = 780 // This will throw an error [TypeError: Assignment to constant variable]
