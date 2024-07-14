1da057b40e212e89276cba6c774111861d1d6a19
TuhinCodeCraft <tuhinghosh0169@gmail.com>
2024-07-14 15:49:06 +0530

    chore: Add arithmetic, assignment, comparison, logical, and ternary operators activities

---
+++ Day02/prog.js
@@ -0,0 +1,38 @@
+// Activity 1: (Arithmatic Operators)
+let a = 5
+let b = 10
+console.log(a+b); // Task 1
+console.log(a-b); // Task 2
+console.log(a*b); // Task 3
+console.log(a/b); // Task 4
+console.log(a%b); // Task 5
+
+// Activity 2: (Assignment Operators)
+let x = 5
+x += 105
+console.log(x); // Task 6
+
+let y = 10
+y -= 4
+console.log(y); // Task 7
+
+// Activity 3: (Comparison Operators)
+// Task 8:
+console.log(4>5);
+console.log(5<6);
+//Task 9:
+console.log(5<=5);
+console.log(10>=4);
+// Task 10:
+console.log(15=='15');
+console.log(15==='15');
+
+// Activity 4: (Logical Operator)
+console.log(4<5 && 5>5); // Task 11
+console.log(4<5 || 5>5); // Task 12
+console.log(!5); // Task 13
+
+// Activity 5: (Ternary Operator)
+const num = 5
+const res = num>0 ? `${num} is positive` : `${num} is not positive`
+console.log(res); // Task 14
